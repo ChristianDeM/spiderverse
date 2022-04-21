f1fe1febeb2d6a0262079a90c50f913f54ec4b31
ChristianDeM <Christian.d.martino@gmail.com>
2022-04-21 01:30:53 -0500

    agregando constructor a spydeman.js                Se agregaron los valores a la clase spiderman.js en este caso el nombe

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -2,6 +2,6 @@ const Spiderman = require('../app/Spiderman')
 describe("Probando clase spyderman", () => {
     test('Caso 1: crando un objeto', () => {
       const Andrew = new Spiderman("spyderman sony",31,"Andrew Garfield","sony")
-        expect(andrew.name).toBe("spyderman sony")
+        expect(Andrew.name).toBe("spyderman sony")
     });
   })
