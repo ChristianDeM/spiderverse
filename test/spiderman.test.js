a17b128600453e8b852d40dac546c21fc4b3d113
ChristianDeM <Christian.d.martino@gmail.com>
2022-04-21 01:33:29 -0500

    Caso 2 tom con error                               Se creo otro caso con error  en tom holand

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -3,5 +3,9 @@ describe("Probando clase spyderman", () => {
     test('Caso 1: crando un objeto', () => {
       const Andrew = new Spiderman("spyderman sony",31,"Andrew Garfield","sony")
         expect(Andrew.name).toBe("spyderman sony")
-    });
+    })
+    test('Caso 2: crando a tom ', () => {
+        const Tom = new Spiderman("Tome holland",31,"Andrew Garfield","sony")
+          expect(Tom.name).toBe("spyderman sony")
+      })
   })
