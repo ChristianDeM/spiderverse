2750f4b5c227214aa90d4bc75c70e6dbf9837020
ChristianDeM <Christian.d.martino@gmail.com>
2022-04-21 01:25:19 -0500

    Agregando spiderman.js  y require en test                            Se realizo spiderman.js con la clase Spiderman   y  el require de spiderman.test

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,7 @@
+const Spiderman = require('../app/Spiderman')
 describe("Probando clase spyderman", () => {
     test('Caso 1: crando un objeto', () => {
-      const Andrew = new Spyderman("spyderman sony",31,"Andrew Garfield","sony")
+      const Andrew = new Spiderman("spyderman sony",31,"Andrew Garfield","sony")
         expect(andrew.name).toBe("spyderman sony")
     });
   })
